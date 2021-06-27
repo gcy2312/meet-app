c404fe053842214f2523fe09af4460a218a6108d
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-27 12:24:39 -0400

    Event,js unit test pass
    set showMore default false, add creator, startTime, Location and Description (description (showMore function))

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+
+  state = {
+    showMore: false
+  }
+
+  showMore() {
+    this.setState({ showMore: !this.state.showMore });
+  }
+
+  showText() {
+    return this.state.showMore ? "Hide Event Details" : "Show Event Details";
+  }
+
+  description(event) {
+    return this.state.showMore ? event.description : "";
+  }
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+
+    return (< div className="event" >
+      <h3>{event.summary}</h3>
+      <p><b>Creator: </b>{event.creator.email}</p>
+      <p><b>Start Time: </b>{event.start.dateTime}</p>
+      <p><b>Location: </b>{event.location}</p>
+      <p className="description">{this.description(event)}</p>
+
+      <button className="details-btn" onClick={() => { this.showMore() }}>this.showText()</button>
+
+    </div >
+    );
   }
 }
 export default Event;
