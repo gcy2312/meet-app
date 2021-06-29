f6d37493904dcef224db40d9c3eadd57571a0c87
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-29 18:07:51 -0400

    import bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
