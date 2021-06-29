a913b23f816d363ab136d23300bb0b1406ac9fa9
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-28 21:31:59 -0400

    set state events, location, numberDisplayed
    componentDidMount - getEvents+ extractLocation
    updateEvents = if all = events, if not, filter with event.location - set currentLocation
    add updateEvents = {this.updateEvents} to CitySearch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
+import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <NumberOfEvents />
-      <CitySearch />
-      <EventList />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    events: [],
+    locations: [],
+    numberDisplayed: 32,
+    // currentLocation: 'all',
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    const { numberDisplayed } = this.state;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({
+          events,
+          locations: extractLocations(events)
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ? events
+        :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents,
+        currentLocation: location
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NumberOfEvents />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
+      </div>
+
+    );
+  }
+
 }
 
 export default App;
