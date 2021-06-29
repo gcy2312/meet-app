09c88b0893111222f46d77cd3004aacade5ddf29
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-29 18:08:38 -0400

    add Row / Col / Form bootstrap

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class NumberOfEvents extends Component {
   state = {
@@ -11,20 +14,27 @@ class NumberOfEvents extends Component {
     this.setState({
       numberDisplayed: value,
     });
+    this.props.updateNumberOfEvents(event.target.value);
   };
 
 
   render() {
+    const numberDisplayed = this.state.numberDisplayed;
     return (
-      <div className="NumberOfEvents">
-        <label htmlFor="numberDisplayed">Events Displayed/Page</label>
-        <input
-          type="number"
-          className="event-number-input"
-          value={this.state.numberDisplayed}
-          onChange={this.handleInputChanged} />
-
-      </div>
+      <Row>
+        <Col md={3}>
+          <div className="NumberOfEvents">
+            <Form>
+              <Form.Label htmlFor="numberDisplayed">Events Displayed/Page</Form.Label><br />
+              <input
+                type="number"
+                className="event-number-input"
+                value={numberDisplayed}
+                onChange={this.handleInputChanged} />
+            </Form>
+          </div>
+        </Col>
+      </Row>
     );
   }
 }
