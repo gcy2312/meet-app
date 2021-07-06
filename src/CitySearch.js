4e20b8954d7da30bf55274c7a7c67da78e11bb4e
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-07-06 18:37:59 -0400

    import InfoAlert, add if statement to handleInputChanged(infoAlert)

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
+
 import Form from 'react-bootstrap/Form';
 import { ListGroup } from 'react-bootstrap';
 
@@ -6,24 +8,35 @@ class CitySearch extends Component {
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: false,
+    infoText: '',
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
-      showSuggestions: false
+      showSuggestions: false,
+      infoText: '',
     });
     this.props.updateEvents(suggestion, this.props.numberDisplayed);
   }
@@ -31,8 +44,10 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        {/* <InfoAlert text={this.state.infoText} /> */}
         <Form>
-          <Form.Label>Filter Events By City</Form.Label>
+          <Form.Label>Filter Events By City: </Form.Label>
+          <InfoAlert text={this.state.infoText} /><br />
           <input
             type="text"
             className="city"
