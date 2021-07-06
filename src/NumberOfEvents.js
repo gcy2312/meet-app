1dc13e79ee900e47c66e2324989bb54b8585a5c4
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-07-06 18:37:01 -0400

    add errorText + Alert to numberDisplayed

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
+
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,15 +8,29 @@ import Col from 'react-bootstrap/Col';
 class NumberOfEvents extends Component {
   state = {
     numberDisplayed: 32,
+    errorText: '',
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
 
-    this.setState({
-      numberDisplayed: value,
-    });
-    this.props.updateNumberOfEvents(event.target.value);
+    if (value < 1) {
+      return this.setState({
+        numberDisplayed: '',
+        errorText: 'Please select a number between 1 and 32',
+      });
+    } else if (value > 32) {
+      return this.setState({
+        numberDisplayed: '',
+        errorText: 'Please select a number between 1 and 32',
+      });
+    } else {
+      this.setState({
+        numberDisplayed: value,
+        errorText: '',
+      });
+      this.props.updateNumberOfEvents(event.target.value);
+    }
   };
 
 
@@ -32,6 +48,8 @@ class NumberOfEvents extends Component {
                 value={numberDisplayed}
                 onChange={this.handleInputChanged} />
             </Form>
+            <ErrorAlert text={this.state.errorText} />
+
           </div>
         </Col>
       </Row>
