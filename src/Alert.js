c727d1d3be92088a9a24683a001d4a9bb91a739d
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-07-06 18:38:33 -0400

    create InfoAlert & ErrorAlert OOP

---
+++ src/Alert.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null;  //children will override later
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert">
+        <p className="alert-message" style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'blue';
+  }
+}
+
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'red';
+  }
+}
+
+export { InfoAlert };
+export { ErrorAlert };
