51a230f34534a21194082ddaed05115436029239
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-07-12 12:47:32 -0400

    update !navigator.onLine

--- src/api.js
+++ src/api.js
@@ -76,9 +76,9 @@ export const getEvents = async () => {
   }
 
   if (!navigator.onLine) {
-    const events = localStorage.getItem('lastEvents');
+    const data = localStorage.getItem('lastEvents');
     NProgress.done();
-    console.log(events);
+    console.log(data);
     return {
       events: JSON.parse(data).events,
       locations: extractLocations(JSON.parse(data).events),
