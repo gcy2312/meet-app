602b2d4cba0ca4b5aa29bd31a8e5b19a735758a2
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-07-15 18:29:52 -0400

    create component EventGenre

---
+++ src/data-visualization/EventGenre.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+
+
+
+
+
+
+export default EventGenre;
